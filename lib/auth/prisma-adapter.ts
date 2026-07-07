309ea488cd842887158993d981782a7d8431b718
Param <enggpsagar@gmail.com>
2026-07-07 15:31:32 +0530

    updatedprosma-adapter package

--- lib/auth/prisma-adapter.ts
+++ lib/auth/prisma-adapter.ts
@@ -1,4 +1,4 @@
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/lib/prisma"
 
 export const adapter = PrismaAdapter(prisma)
